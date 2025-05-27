394381d8af2c02bb3dba13b5442b04a830a75ac8
Gabriel Santos <gabriels.santosx@gmail.com>
2025-05-27 00:31:39 -0300

    fix: validação ao realizar um novo cadastro

--- src/EstoqueFlow.Application/UseCases/Usuarios/Registrar/RegistrarUsuarioUseCase.cs
+++ src/EstoqueFlow.Application/UseCases/Usuarios/Registrar/RegistrarUsuarioUseCase.cs
@@ -12,11 +12,23 @@ public class RegistrarUsuarioUseCase(
 {
     public async Task<UsuarioResponse> Executar(UsuarioRequest request)
     {
+        await ValidarDados(request);
+
         var entidade = request.ToEntidade();
         entidade.Senha = criptografarSenha.Criptografar(request.Senha);
 
         var usuario = await usuarioRepository.Adicionar(entidade);
 
         return usuario.ToDto();
+    }
+
+    private async Task ValidarDados(UsuarioRequest request)
+    {
+        var usuarioExistente = await usuarioRepository.ObterUsuarioPorEmail(request.Email);
+
+        if (usuarioExistente is not null)
+        { 
+            throw new InvalidOperationException("Já existe uma conta ativa com esse e-mail.");
+        }
     }
 }
